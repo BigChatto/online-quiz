2f4850e6e7b0af430386a2fb068fc2e3ba461924
BigChatto <>
2023-08-20 18:39:55 +0000

    Add style and make buttons active

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+const startBtn = document.getElementById("start-btn");
+const popUp = document.getElementById("pop-up");
+const exitBtn = document.querySelector(".exit-btn");
+const main = document.getElementById("main");
+const continueBtn = document.querySelector(".continue-btn");
+const quizPart = document.getElementById("quiz-part");
+
+startBtn.onclick = () => {
+    popUp.classList.add("active");
+    main.classList.add("active");
+
+};
+
+exitBtn.onclick = () => {
+    popUp.classList.remove("active");
+    main.classList.remove("active");
+
+};
+
+continueBtn.onclick = () => {
+    quizPart.classList.add("active");
+    popUp.classList.remove("active");
+    main.classList.remove("active");
+
+};
